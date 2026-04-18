32d8b3ffbaac778bf48ff0eea0c5689c4580c4a8
IntrovertSoul17 <raiaryan17.2006@gmail.com>
2026-04-18 16:12:24 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,8 @@
+set2 = {10,11,12}
+set_union = set.union(set2)
+print("union of set and set 2 :",set_union)
+set3 = {5,6,7,10}
+set_intersection = set.intersection(set3)
+print("intersection of set and set 3:",set_intersection)
+set_difference = set.difference (set3)
+print("difference of set and set 3:", set_difference)
---
+++ Assignment4.py
@@ -0,0 +1,26 @@
+dictionary = {'name': 'Riya', 'age':18, 'city':'pune'}
+print(dictionary)
+print("accessing name from dictionary :", dictionary['name'])
+dictionary['country'] = 'India'
+print("dictionary after adding country :", dictionary)
+dictionary.pop('age')
+print("dictionary after removing age :", dictionary)
+dictionary['age'] = 19 
+print("dictionary after updating age :", dictionary)
+keys = dictionary.keys()
+print("keys of dictionary :", keys)
+dictionary.pop('city')
+print("dictionary after removing city :", dictionary)
+dictionary = {'name': 'Riya', 'age':18, 'city':'pune'}
+print(dictionary)
+print("accessing name from dictionary :", dictionary['name'])
+dictionary['country'] = 'India'
+print("dictionary after adding country :", dictionary)
+dictionary.pop('age')
+print("dictionary after removing age :", dictionary)
+dictionary['age'] = 19 
+print("dictionary after updating age :", dictionary)
+keys = dictionary.keys()
+print("keys of dictionary :", keys)
+dictionary.pop('city')
+print("dictionary after removing city :", dictionary)
